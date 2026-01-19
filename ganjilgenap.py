3c47487205070b831e630cc467ef8ab5977b55bb
zahruddeen <akuasyfi@gmail.com>
2026-01-19 01:11:59 +0000

    versi 1.0 kode ganjil genap

---
+++ ganjilgenap.py
@@ -0,0 +1,8 @@
+num = int(input('masukkan angka dong pun:'))
+
+if num % 2 ==0:
+    print(num, 'genap inimah dong')
+else:
+    print('lah', num, 'mah ganjil')
+
+print('ini kode diriku ini dari codespace dong pun')
